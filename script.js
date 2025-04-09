11d7acc3f52b8faafe88fc49e52f173be556d52e
Ibrahim BAH ODK 2025 <babrahim276y@gmail.com>
2025-04-09 14:45:15 +0000

    Dixième commit

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// const getElementsToChange = document.querySelectorAll(".left-column", ".right-column");
+
+// document.getElementById("toggle-theme").addEventListener("click", () => {
+//   getElementsToChange.forEach((columnSide, index) => {
+//     columnSide.style.backgroundColor = "#848484"
+//   })
+// })
+
+const leftColumn = document.querySelector(".left-column");
+const rightColumn = document.querySelector(".right-column");
+
+document.getElementById("toggle-theme").addEventListener("click", () => {
+  leftColumn.style.backgroundColor = "#848484"; // couleur personnalisée
+   rightColumn.style.backgroundColor = "green"; // vert
+});
+
+// const profiles = document.querySelectorAll('.left-column');
+
+// document.getElementById("toggle-theme").addEventListener("click", () => {
+//   // Changer le texte + couleur de fond pour chaque élément
+//   profiles.forEach((profile, index) => {
+
+//     profile.style.backgroundColor = "#a0d2eb";     // Changer la couleur de fond
+//   });
+// });
+
 // Thème alternatif
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -15,5 +41,5 @@ document.addEventListener('DOMContentLoaded', function () {
         window.print(); // ouvre la fenêtre d'impression (PDF possible)
       });
     }
-  });
+});
   
