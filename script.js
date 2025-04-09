a378c99e513ff2d0071b1eb4d07af75c860ac8e1
Ibrahim BAH ODK 2025 <babrahim276y@gmail.com>
2025-04-09 17:20:37 +0000

    Onzième commit

--- script.js
+++ script.js
@@ -1,45 +1,12 @@
-// const getElementsToChange = document.querySelectorAll(".left-column", ".right-column");
-
-// document.getElementById("toggle-theme").addEventListener("click", () => {
-//   getElementsToChange.forEach((columnSide, index) => {
-//     columnSide.style.backgroundColor = "#848484"
-//   })
-// })
-
 const leftColumn = document.querySelector(".left-column");
 const rightColumn = document.querySelector(".right-column");
 
 document.getElementById("toggle-theme").addEventListener("click", () => {
-  leftColumn.style.backgroundColor = "#848484"; // couleur personnalisée
-   rightColumn.style.backgroundColor = "green"; // vert
+  leftColumn.style.backgroundColor = "#848484"; 
+   rightColumn.style.backgroundColor = "green";
 });
 
-// const profiles = document.querySelectorAll('.left-column');
-
-// document.getElementById("toggle-theme").addEventListener("click", () => {
-//   // Changer le texte + couleur de fond pour chaque élément
-//   profiles.forEach((profile, index) => {
-
-//     profile.style.backgroundColor = "#a0d2eb";     // Changer la couleur de fond
-//   });
-// });
-
-// Thème alternatif
-
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleBtn = document.getElementById('toggle-theme');
-    const downloadBtn = document.getElementById('download-pdf');
-  
-    if (toggleBtn) {
-      toggleBtn.addEventListener('click', function () {
-        document.documentElement.classList.toggle('alt-theme');
-      });
-    }
-  
-    if (downloadBtn) {
-      downloadBtn.addEventListener('click', function () {
-        window.print(); // ouvre la fenêtre d'impression (PDF possible)
-      });
-    }
+document.getElementById('download-pdf').addEventListener('click', function() {
+  const element = document.querySelector('all-print');
+  html2pdf().from(element).save('mon_cv.pdf');
 });
-  
